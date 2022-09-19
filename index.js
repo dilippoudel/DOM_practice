c231326555a48d3aa445b148210196c3d6a9c55f
Dilip <dilippoudel578@gmail.com>
2022-09-19 19:29:03 +0300

    Add new Rows in table

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 let elements = document.getElementsByName('firstName')
 console.log(elements[0].value)
 console.log(elements[1].value)
+
+// create a function to add new rows to the table
+function addNewRows() {
+  let tableElement = document.querySelector('#newTable>tbody')
+  let newRow = tableElement.insertRow()
+  newRow.insertCell(0).append('New Cell')
+  newRow.insertCell(1).append('New Cell')
+}
+const btnElement = document.getElementById('btn')
+btnElement.addEventListener('click', addNewRows)
